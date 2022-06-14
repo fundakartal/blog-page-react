f04e247aa7c39800d82c605074ce9ae64482d735
Funda Kartal <kartalfunda01@gmail.com>
2022-06-14 17:21:23 +0300

    initial

---
+++ components/header.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+function Header() {
+  return (
+    <header className='container py-6'>
+      <nav className='space-x-4'>
+        <Link href='/'>
+          <a>About</a>
+        </Link>
+        <Link href='/blog'>
+          <a>Blog</a>
+        </Link>
+      </nav>
+    </header>
+  )
+}
+export default Header
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+function HomePage() {
+  return (
+    <>
+      <div className='container'>
+        <div className='space-y-4'>
+          <h1 className='text-2xl font-bold'>
+            Lorem, ipsum dolor. <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus
+            recusandae nesciunt maxime temporibus voluptas.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            error quis expedita aliquam sunt. Delectus?
+          </p>
+        </div>
+      </div>
+      <div className='container-xl mt-20'>
+        <img src='/desk.jpg' alt='desk' />
+      </div>
+    </>
+  )
+}
+export default HomePage
