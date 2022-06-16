045b4f2e26dc07456ab778239d1ca6ea1e3e60c0
Funda Kartal <kartalfunda01@gmail.com>
2022-06-16 23:48:46 +0300

    Edit blog posts

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -5,6 +5,7 @@ import path from 'path'
 import matter from 'gray-matter'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import Button from '../../components/button'
+import Link from 'next/link'
 
 export const getStaticPaths = async () => {
   const files = fs.readdirSync(path.join('posts'))
@@ -39,11 +40,30 @@ export const getStaticProps = async ({ params: { slug } }) => {
   }
 }
 
-const PostPage = ({ frontMatter: { title }, mdxSource }) => {
+const PostPage = ({ frontMatter, mdxSource }) => {
+  const { title, description, tags } = frontMatter
   return (
     <div className='container-2xl mt-4'>
-      <h1 className='text-2xl font-bold mb-10'>{title}</h1>
-      <MDXRemote {...mdxSource} components={{ Button, SyntaxHighlighter }} />
+      <article className='space-y-6'>
+        <h1 className='text-4xl font-bold'>{title}</h1>
+        <p>{description}</p>
+        <p>
+          {tags.map((tag) => (
+            <Link href={`/blog?tag=${tag.toLowerCase()}`} key={tag}>
+              <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+                {tag}
+              </a>
+            </Link>
+          ))}
+        </p>
+        <hr />
+        <div className='prose'>
+          <MDXRemote
+            {...mdxSource}
+            components={{ Button, SyntaxHighlighter }}
+          />
+        </div>
+      </article>
     </div>
   )
 }
