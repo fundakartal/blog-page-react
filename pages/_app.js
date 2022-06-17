d3481ecb82a252d47cf3ed42f7cc3a36472107aa
Funda Kartal <kartalfunda01@gmail.com>
2022-06-18 00:06:36 +0300

    Add Head component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import Header from '../components/header'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className='antialiased text-gray-700'>
+      <Head>
+        <title>My Blog</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Header />
       <main className='mt-6 mb-20'>
         <Component {...pageProps} />
