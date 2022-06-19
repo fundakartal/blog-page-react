a07723c48262fac59eddc0bbf0eccdae5eb811e0
Funda Kartal <kartalfunda01@gmail.com>
2022-06-19 17:21:18 +0300

    Edit post component

--- components/post.js
+++ components/post.js
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-export default function Post({post, index}) {
+export default function Post({ post, index }) {
+  const { title, description, date, tags, thumbnailUrl } = post.frontMatter
   return (
     <Link href={'/blog/' + post.slug} passHref key={index}>
-      <a className='flex flex-col md:flex-row max-w-[500px] md:max-w-xl rounded-lg bg-white shadow-lg'>
+      <a className='flex flex-col md:flex-row max-w-[500px] md:max-w-full rounded-lg bg-white shadow-lg'>
         <div className='md:max-w-[50%] text-[0]'>
           <Image
-            src={post.frontMatter.thumbnailUrl}
+            src={thumbnailUrl}
             className='rounded-t-lg md:rounded-tr-none md:rounded-l-lg w-fill'
             alt='thumbnail'
             width={500}
@@ -15,15 +16,20 @@ export default function Post({post, index}) {
             objectFit='cover'
           />
         </div>
-        <div className='p-6 flex flex-col '>
-          <h5 className='text-gray-900 text-xl font-medium mb-2'>
-            {post.frontMatter.title}
-          </h5>
-          <p className='text-gray-700 text-base mb-4'>
-            {post.frontMatter.description}
+        <article className='p-6 flex flex-col gap-y-4'>
+          <h5 className='text-gray-900 text-xl font-medium'>{title}</h5>
+          <p className='text-gray-700 text-base'>{description}</p>
+          <p>
+            {tags.map((tag) => (
+              <Link href={`/tag/${tag.toLowerCase()}`} key={tag}>
+                <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+                  {tag.toLowerCase()}
+                </a>
+              </Link>
+            ))}
           </p>
-          <p className='text-gray-600 text-xs'>{post.frontMatter.date}</p>
-        </div>
+          <p className='text-gray-600 text-xs'>{date}</p>
+        </article>
       </a>
     </Link>
   )
