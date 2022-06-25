217be2db5f3a6caaefa92200f90437a263c9b716
Funda Kartal <kartalfunda01@gmail.com>
2022-06-25 16:35:18 +0300

    Add auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,29 @@
 import Header from '../components/header'
 import '../styles/globals.css'
 import Head from 'next/head'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className='antialiased text-gray-700'>
-      <Head>
-        <title>My Blog</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Header />
-      <main className='mt-6 mb-20'>
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <Auth0Provider
+      domain='dev-8tbdid6t.us.auth0.com'
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <div className='antialiased text-gray-700'>
+        <Head>
+          <title>My Blog</title>
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
+        </Head>
+        <Header />
+        <main className='mt-6 mb-20'>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </Auth0Provider>
   )
 }
 
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -6,6 +6,71 @@ import matter from 'gray-matter'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import Button from '../../components/button'
 import Link from 'next/link'
+import { useAuth0 } from '@auth0/auth0-react'
+
+export default function PostPage({ frontMatter, mdxSource, slug }) {
+  const { title, description, tags } = frontMatter
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
+  return (
+    <div className='container-2xl mt-4'>
+      <article className='space-y-6'>
+        <h1 className='text-4xl font-bold'>{title}</h1>
+        <p>{description}</p>
+        <p>
+          {tags.map((tag) => (
+            <Link href={`/tag/${tag.toLowerCase()}`} key={tag}>
+              <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+                {tag.toLowerCase()}
+              </a>
+            </Link>
+          ))}
+        </p>
+        <hr />
+        <div className='prose'>
+          <MDXRemote
+            {...mdxSource}
+            components={{ Button, SyntaxHighlighter }}
+          />
+        </div>
+      </article>
+      <form className='mt-10'>
+        <textarea
+          rows='2'
+          className='border border-gray-300 w-full rounded block px-2 py-1'
+        ></textarea>
+        <div className='mt-4'>
+          {isAuthenticated ? (
+            <div className='flex items-center justify-between gap-x-2'>
+              <button className='bg-blue-600 px-2 py-1 rounded text-white'>
+                Send
+              </button>
+              <div className='flex items-center gap-x-2'>
+                <img src={user.picture} width={40} className='rounded-full' />
+                <span className='font-semibold'>{user.name}</span>
+                <button
+                  onClick={() =>
+                    logout({
+                      returnTo: `${process.env.NEXT_PUBLIC_URL}/blog/${slug}`,
+                    })
+                  }
+                >
+                  &#128473;
+                </button>
+              </div>
+            </div>
+          ) : (
+            <button
+              className='bg-blue-600 px-2 py-1 rounded text-white'
+              onClick={() => loginWithRedirect()}
+            >
+              Login
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
+  )
+}
 
 export const getStaticPaths = async () => {
   const files = fs.readdirSync(path.join('posts'))
@@ -39,33 +104,3 @@ export const getStaticProps = async ({ params: { slug } }) => {
     },
   }
 }
-
-const PostPage = ({ frontMatter, mdxSource }) => {
-  const { title, description, tags } = frontMatter
-  return (
-    <div className='container-2xl mt-4'>
-      <article className='space-y-6'>
-        <h1 className='text-4xl font-bold'>{title}</h1>
-        <p>{description}</p>
-        <p>
-          {tags.map((tag) => (
-            <Link href={`/tag/${tag.toLowerCase()}`} key={tag}>
-              <a className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-                {tag.toLowerCase()}
-              </a>
-            </Link>
-          ))}
-        </p>
-        <hr />
-        <div className='prose'>
-          <MDXRemote
-            {...mdxSource}
-            components={{ Button, SyntaxHighlighter }}
-          />
-        </div>
-      </article>
-    </div>
-  )
-}
-
-export default PostPage
